ff39d7cdfa17915cce43660322d8a6a66b86a412
AlbertoCavans <alberto96cavani@gmail.com>
2024-04-09 20:39:08 +0200

    Feat - allign repo

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -25,7 +25,8 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view("admin.projects.create");
+        $project = new Project;
+        return view("admin.projects.form", compact("project"));
     }
 
     /**
@@ -62,7 +63,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        return view("admin.projects.edit", compact("project"));
+        return view("admin.projects.form", compact("project"));
     }
 
     /**
